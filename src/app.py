d7787386d6d967011b98831cd62bb8231eca0be9
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-07 13:10:53 +0000

    refactor

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from routes.file_uplaoder_router import file_bp
+from middlewares.file_uplaoder_middleware import file_upload_middleware
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
 app.config['UPLOAD_FOLDER'] = 'C:\\Users\\Siddharth\\Desktop\\File_Uploader'
+
+
+app.before_request(file_upload_middleware)
+app.register_blueprint(file_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , debug= True)
