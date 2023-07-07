6154ffb15da40d73cadedfaef154d5790c041a1e
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-07 13:05:20 +0000

    Added contoller

---
+++ src/controller/file_uploader_controller.py
@@ -0,0 +1,22 @@
+from flask import  request , make_response
+from models.file_uploader_model import File
+from werkzeug.utils import secure_filename
+import os
+import bson.json_util as json_util
+from app import app
+
+
+def file_upload():
+    file = request.files['file']
+    
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        new_file = File(filename)
+        saved_file = new_file.save_file()
+        json_version = json_util.dumps(saved_file)
+        return make_response({'message':'File uploaded succesfully' , 'file': json_version} , 201)
+    
+    return make_response({'message': 'Invalid file'} , 400)
+        
