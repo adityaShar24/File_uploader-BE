1f653dd4b7b2054228b764db98b725ecf07d922e
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-07 13:01:00 +0000

    Added get_all_files function in file_uplaoder_model

--- src/models/file_uploader_model.py
+++ src/models/file_uploader_model.py
@@ -7,3 +7,8 @@ class File:
     def save_file(self):
         saved_file = files_collection.insert_one({'filename': self.filename}).inserted_id
         return saved_file
+
+    def get_all_files():
+        files = files_collection.find()
+        files_list = list(files)
+        return files_list   
