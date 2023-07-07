942a4944d8271dc5f6a16e3dae99dd22ed56a22f
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-07 13:06:40 +0000

    refactor

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
+app.config['UPLOAD_FOLDER'] = 'C:\\Users\\Siddharth\\Desktop\\File_Uploader'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , debug= True)
