42de7d539f519fbb428726b4891aa2dcbecba99f
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-08 07:02:13 +0000

    Added login_user_middleware function

--- src/middlewares/user_middleware.py
+++ src/middlewares/user_middleware.py
@@ -12,3 +12,21 @@ def register_user_middleware():
     
     if not password:
         return make_response({'message':'please enter password'} , 400)
+    existing_user = User.find_by_username(username)
+    if existing_user:
+        return make_response({'message':'username aleardy exists please enter unique username'} , 400)
+    
+    
+def login_user_middleware():
+    body = json.loads(request.data)
+    username = body['username']
+    password = body['password']
+    if not username:
+        return make_response({'message':'please enter username'} , 400)
+    
+    if not password:
+        return make_response({'message':'please enter password'} , 400)
+
+    existing_user = User.find_by_username(username)
+    if not existing_user:
+        return make_response({'message':'cannot find user please enter username correctly'} , 400)
