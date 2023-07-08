a6be309c1a6d9d39f1e081ee5c38fe3fbf7481f5
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-08 06:52:01 +0000

    added find_by_username function

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,10 +1,14 @@
 from database.mongo import users_collection
 
 class User:
-    def __init(self , username , password):
+    def __init__(self , username , password):
         self.username = username
         self.password = password
         
     def save_users(self):
         user_id = users_collection.insert_one({'username':self.username , 'password':self.password}).inserted_id
         return user_id
+
+    def find_by_username(username):
+        user = users_collection.find_one({'username': username})
+        return user
