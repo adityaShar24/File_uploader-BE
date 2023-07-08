21e404f1db62a5424fb20515b787d9d4d1ffc08c
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-08 06:28:23 +0000

    added get_all_files in file_uploader_controller

--- src/controller/file_uploader_controller.py
+++ src/controller/file_uploader_controller.py
@@ -3,7 +3,7 @@ from models.file_uploader_model import File
 from werkzeug.utils import secure_filename
 import os
 import bson.json_util as json_util
-from app import app
+# from app import app
 
 
 def file_upload():
@@ -11,7 +11,7 @@ def file_upload():
     
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
         new_file = File(filename)
         saved_file = new_file.save_file()
@@ -19,4 +19,9 @@ def file_upload():
         return make_response({'message':'File uploaded succesfully' , 'file': json_version} , 201)
     
     return make_response({'message': 'Invalid file'} , 400)
-        
+
+
+def get_all_files():
+    files = File.get_all_files()
+    json_Version = json_util.dumps(files)
+    return make_response(json_Version , 201)
