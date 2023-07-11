7a1824d95400f9a64b8f08a89f8b3170a997bd2d
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-11 10:56:12 +0000

    Added gridfs functionalities

--- src/database/mongo.py
+++ src/database/mongo.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
+from gridfs import GridFS
 
 MONGO_CONNECTION_STRING = "mongodb+srv://aditya:aditya2004@cluster0.lgjqzvz.mongodb.net/?retryWrites=true&w=majority"
 
 mongoclient = MongoClient(MONGO_CONNECTION_STRING)
 
 Database = mongoclient['File_uplaoder']
-files_collection = Database['files']
+fs = GridFS(Database , collection= 'files')
+users_collection = Database['Users'] 
 
 try:
     mongoclient.server_info()
-    print("Connection to mongoDB successfull!")
+    print("Connection to MongoDb Successful!")
 except Exception as e:
-    print("Failed to connect" , e)
+    print("Failed to Connect" , e)
+
