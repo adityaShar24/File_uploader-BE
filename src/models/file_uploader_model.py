ef47bef888474d2ca72e239b84f96ea07a685174
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-11 10:53:17 +0000

    refactor

--- src/models/file_uploader_model.py
+++ src/models/file_uploader_model.py
@@ -1,14 +1,14 @@
-from database.mongo import files_collection
+from database.mongo import fs
 
 class File:
     def __init__(self , filename ):
         self.filename = filename
         
     def savefile(self , file_data):
-        saved_file = files_collection.insert_one({'filename': self.filename , 'file_data': file_data}).inserted_id
+        saved_file = fs.put(file_data, filename=self.filename)
         return saved_file
     
     def get_all_files():
-        files = files_collection.find()
+        files = fs.find()
         files_list = list(files)
         return files_list
