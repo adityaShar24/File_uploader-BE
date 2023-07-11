3a3c5bc492f2f449830c885e63759d7417ea6b9e
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-11 07:11:44 +0000

    refactor

--- src/models/file_uploader_model.py
+++ src/models/file_uploader_model.py
@@ -4,11 +4,11 @@ class File:
     def __init__(self , filename ):
         self.filename = filename
         
-    def save_file(self):
-        saved_file = files_collection.insert_one({'filename': self.filename}).inserted_id
+    def savefile(self , file_data):
+        saved_file = files_collection.insert_one({'filename': self.filename , 'file_data': file_data}).inserted_id
         return saved_file
-
+    
     def get_all_files():
         files = files_collection.find()
         files_list = list(files)
-        return files_list   
+        return files_list
