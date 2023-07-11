bb3594282355a96a9f3e5d0090687cd76932534c
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-11 10:52:17 +0000

    refactor

--- src/middlewares/user_middleware.py
+++ src/middlewares/user_middleware.py
@@ -4,29 +4,31 @@ import json
 
 
 def register_user_middleware():
-    body = json.loads(request.data)
-    username = body['username']
-    password = body['password']
-    if not username:
-        return make_response({'message':'please enter username'} , 400)
-    
-    if not password:
-        return make_response({'message':'please enter password'} , 400)
-    existing_user = User.find_by_username(username)
-    if existing_user:
-        return make_response({'message':'username aleardy exists please enter unique username'} , 400)
+    if request.endpoint == 'auth_bp.register_wrapper':
+        body = json.loads(request.data)
+        username = body['username']
+        password = body['password']
+        if not username:
+            return make_response({'message':'please enter username'} , 400)
+        
+        if not password:
+            return make_response({'message':'please enter password'} , 400)
+        existing_user = User.find_by_username(username)
+        if existing_user:
+            return make_response({'message':'username aleardy exists please enter unique username'} , 400)
     
     
 def login_user_middleware():
-    body = json.loads(request.data)
-    username = body['username']
-    password = body['password']
-    if not username:
-        return make_response({'message':'please enter username'} , 400)
-    
-    if not password:
-        return make_response({'message':'please enter password'} , 400)
+    if request.endpoint == 'auth_bp.login_wrapper':
+        body = json.loads(request.data)
+        username = body['username']
+        password = body['password']
+        if not username:
+            return make_response({'message':'please enter username'} , 400)
+        
+        if not password:
+            return make_response({'message':'please enter password'} , 400)
 
-    existing_user = User.find_by_username(username)
-    if not existing_user:
-        return make_response({'message':'cannot find user please enter username correctly'} , 400)
+        existing_user = User.find_by_username(username)
+        if not existing_user:
+            return make_response({'message':'cannot find user please enter username correctly'} , 400)
