1492e4fecbd62915de3b9a2b10c95f637a87cdd6
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-14 16:06:48 +0000

    added get_dp

--- src/controller/file_uploader_controller.py
+++ src/controller/file_uploader_controller.py
@@ -1,8 +1,9 @@
-from flask import  request , make_response , send_file
+from flask import  request , make_response, send_file
+from database.mongo import fs , users_collection
 from models.file_uploader_model import File
 from models.user_model import User
 import bson.json_util as json_util
-
+from bson.objectid import ObjectId
 
 
 def file_upload():
@@ -16,12 +17,20 @@ def file_upload():
     
     return make_response({'message': 'Invalid file'} , 400)
 
+def get_file(filename):
+    file = fs.find_one({ "filename": filename })
+    return send_file(file, mimetype="image/png")
+
+
+def get_dp(userId , filename):
+    user_file = users_collection.find_one({"_id": ObjectId(userId) , "filename": filename})
+    print(user_file)
+    file = fs.find_one({ "filename": filename })
+    if file.filename == user_file['filename']:
+        return send_file(file , mimetype="image/png")
+    return make_response({"message":"No file found"} , 404)
 
 def get_all_files():
     files = File.get_all_files()
     json_Version = json_util.dumps(files)
     return make_response(json_Version , 201)
-
-def get_file(filename):
-    file = fs.find_one({ "filename": filename })
-    return send_file(file, mimetype="image/png")
