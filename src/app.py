dca343dd37876daabb4edd01105b680418169fb5
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-14 16:14:25 +0000

    instantiated with JWT

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from routes.file_uplaoder_router import file_bp
+from flask_jwt_extended import JWTManager
+from routes.file_uploader_router import file_bp
 from routes.user_router import auth_bp
-from middlewares.file_uplaoder_middleware import file_upload_middleware
+from middlewares.file_uploader_middleware import file_upload_middleware
 from middlewares.users_middeware import login_user_middleware , register_user_middleware
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
 app.config['UPLOAD_FOLDER'] = 'C:\\Users\\Siddharth\\Desktop\\File_Uploader'
 
-
+JWTManager(app)
 app.before_request(file_upload_middleware)
 app.before_request(register_user_middleware)
 app.before_request(login_user_middleware)
