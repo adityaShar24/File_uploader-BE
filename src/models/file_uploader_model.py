c29ca63f6b0bce9ddfabd023069406645773a65d
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-14 16:09:02 +0000

    added dp

--- src/models/file_uploader_model.py
+++ src/models/file_uploader_model.py
@@ -1,18 +1,22 @@
-from database.mongo import fs , users_collection
-
+from database.mongo import  fs , users_collection
 class File:
-    def __init__(self , filename ):
+    def __init__(self , filename):
         self.filename = filename
         
     def savefile(self , file_data):
         saved_file = fs.put(file_data, filename=self.filename)
         return saved_file
     
+    def dp(userId):
+        user_file = users_collection.find_one({'_id': userId})
+        print(user_file)
+        return user_file
+    
     def send_file(self):
         existing_file = fs.find_one({ "filename": self.filename })
         return existing_file
-
-    
+        
+        
     def get_all_files():
         files = fs.find()
         files_list = list(files)
